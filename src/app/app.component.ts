a2d54ac504b6b1b4fcfad2bbdaf8d553c6a7cd6f
Kenny <75293069+Mafesto8798@users.noreply.github.com>
2022-09-28 21:46:52 -0400

    final push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './menu-item';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'menu';
+  menuItems: MenuItem[] = [
+    { name: 'Chicken Fingers', category: 'dinner', price: 11.99 },
+    { name: 'Pizza', category: 'dinner', price: 11.99 },
+    { name: 'Wings', category: 'sides', price: 8.99 },
+    { name: 'Breadsticks', category: 'sides', price: 4.99 },
+    { name: 'Caesar Salad', category: 'salads', price: 5.99 },
+    { name: 'Cinnamon Roll', category: 'dessert', price: 8.99 },
+  ];
 }
